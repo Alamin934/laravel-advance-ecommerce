0082886ed88e1d52d24645f9a4a83a2cb3dce685
Alamin934 <islamalamin934@gmail.com>
2024-01-30 21:44:18 +0600

    product added with single and multiple images

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Models\Product;
 use App\Models\{Category, SubCategory, ChildCategory, Brand};
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class ProductController extends Controller
 {
@@ -33,9 +36,83 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $file = $request->file('images');
-        // $extension = $file->getClientOriginalExtension();
-        dd($file);
+        $validated = $request->validate([
+            'title' => 'required|unique:products|max:255',
+            'code' => 'required|unique:products',
+            'sub_category' => 'required',
+            'sub_category' => 'required',
+            'purchase_price' => 'required|numeric',
+            'stock_quantity' => 'required|numeric',
+            'description' => 'required',
+            'thumbnail' => 'required|file|image',
+        ]);
+        
+
+        // Thumbnail
+        $thumbnail = '';
+        if($request->file('thumbnail')){
+            $file = $request->file('thumbnail');
+            $original_name = $file->getClientOriginalName();
+            $extension = $file->getClientOriginalExtension();
+            $new_file_name = preg_split("/[\s\-\.]+/", $original_name)[0].'-'.date('d-m-Y-H-i-s').'.'.$extension;
+            $thumbnail = $new_file_name;
+
+            $manager = new ImageManager(new Driver());
+            $image = $manager->read($file);
+            $image->contain(650, 450);
+            $image->toPng()->save('admin/assets/files/products/'.$new_file_name);
+        }
+        
+        // Gallery Images
+        $images = [];
+        if($request->hasfile('images')){
+            $files = $request->file('images');
+            $count = 1;
+            foreach ($files as $file) {
+                $original_name = preg_split("/[\s\-\.]+/", $file->getClientOriginalName())[0];
+                if($original_name){
+                    $original_name = $original_name.$count++;
+                    $new_file_name = $original_name.'-'.date('d-m-Y-H-i-s').'.'.$file->extension();
+                    $images[] = $new_file_name;
+
+                    // store images in folder
+                    $manager = new ImageManager(new Driver());
+                    $image = $manager->read($file);
+                    $image->contain(650, 450);
+                    $image->toPng()->save('admin/assets/files/products/'.$new_file_name);
+                }
+
+            }
+
+        }
+
+        // Store Product in database
+        $sub_category = SubCategory::find($request->sub_category);
+        $product = Product::create([
+            'user_id' => auth()->user()->id,
+            'category_id' => $sub_category->category_id,
+            'sub_category_id' => $request->sub_category,
+            'child_category_id' => $request->child_category,
+            'brand_id' => $request->brand,
+            'title' => $request->title,
+            'slug' => Str::slug($request->title, '-'),
+            'description' => $request->description,
+            'thumbnail' => $thumbnail,
+            'images' => $images,
+            'code' => $request->code,
+            'unit' => $request->unit,
+            'tags' => $request->tags,
+            'purchase_price' => $request->purchase_price,
+            'selling_price' => $request->selling_price,
+            'discount_price' => $request->discount_price,
+            'video' => $request->video,
+            'stock_quantity' => $request->stock_quantity,
+            'featured' => $request->featured,
+            'status' => $request->status,
+        ]);
+
+        $notification = ['message'=>'Product Added successfully', 'alert-type'=>'success'];
+        return redirect()->back()->with($notification);
 
     }
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,8 @@ class Product extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected $casts = [
+        'images' => 'array'
+    ];
 }
